e1fd3a560b395cf328ca7098bafcb9e5ac0ccec1
Anubhav Mittal <anubhav11march@gmail.com>
2019-06-27 13:58:55 +0530

    SMS Retriever API working successfully

--- app/src/main/java/com/example/login/smsBroadcast.java
+++ app/src/main/java/com/example/login/smsBroadcast.java
@@ -11,15 +11,16 @@ import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.common.api.Status;
 
 public class smsBroadcast extends BroadcastReceiver {
-    private OtpListener mlistener = null;
+    private static OtpListener mlistener = null;
     private String otp = "";
-    public void bindListener(OtpListener listener) {
+    public static void bindListener(OtpListener listener) {
         Log.v("AAA", "Binded Listener");
-        this.mlistener= listener;
+        mlistener= listener;
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.v("AAA", "onReceive");
         if(SmsRetriever.SMS_RETRIEVED_ACTION.equals(intent.getAction())){
             Bundle extras = intent.getExtras();
             Status status = (Status) extras.get(SmsRetriever.EXTRA_STATUS);
@@ -27,9 +28,9 @@ public class smsBroadcast extends BroadcastReceiver {
                 case CommonStatusCodes
                         .SUCCESS:
                     String message = (String) extras.get(SmsRetriever.EXTRA_SMS_MESSAGE);
-                    Log.v("AAA", message);
+                    Log.v("AAAB", message);
                     if(mlistener!=null){
-                        for(int i=13; i<19; i++){
+                        for(int i=17; i<23; i++){
                             otp+=message.charAt(i);
                         }
                         Log.v("AAA", otp);
@@ -38,6 +39,7 @@ public class smsBroadcast extends BroadcastReceiver {
 
                     break;
                 case CommonStatusCodes.TIMEOUT:
+                    Log.v("AAA","Timed out");
                     break;
             }
         }
